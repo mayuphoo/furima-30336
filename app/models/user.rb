b9cc64e2e5c4151e24a64cf55140078776482a39
mayuphoo <76111639+mayuphoo@users.noreply.github.com>
2021-01-22 22:01:10 +0900

    rubocop実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,18 +5,18 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates :password, format: { with:VALID_PASSWORD_REGEX}
-  
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
+
   validates :last_name, presence: true
   validates :first_name, presence: true
   VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/
-  validates :last_name, format: { with:VALID_NAME_REGEX}
-  validates :first_name, format: { with:VALID_NAME_REGEX}
+  validates :last_name, format: { with: VALID_NAME_REGEX }
+  validates :first_name, format: { with: VALID_NAME_REGEX }
   validates :last_kana, presence: true
   validates :first_kana, presence: true
   VALID_NAME_KANA_REGEX = /\A[ァ-ヶー－]+\z/
-  validates :last_kana, format: { with: VALID_NAME_KANA_REGEX}
-  validates :first_kana, format: { with: VALID_NAME_KANA_REGEX}
+  validates :last_kana, format: { with: VALID_NAME_KANA_REGEX }
+  validates :first_kana, format: { with: VALID_NAME_KANA_REGEX }
   validates :birthday, presence: true
 end

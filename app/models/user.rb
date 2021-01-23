96ed1fa049349e5976cea0793db363c1aa16a737
mayuphoo <76111639+mayuphoo@users.noreply.github.com>
2021-01-23 11:17:49 +0900

    userモデル訂正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,19 +4,26 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
-  validates :password, format: { with: VALID_PASSWORD_REGEX }
 
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/
-  validates :last_name, format: { with: VALID_NAME_REGEX }
-  validates :first_name, format: { with: VALID_NAME_REGEX }
-  validates :last_kana, presence: true
-  validates :first_kana, presence: true
-  VALID_NAME_KANA_REGEX = /\A[ァ-ヶー－]+\z/
-  validates :last_kana, format: { with: VALID_NAME_KANA_REGEX }
-  validates :first_kana, format: { with: VALID_NAME_KANA_REGEX }
-  validates :birthday, presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :last_name
+    validates :first_name
+    validates :last_kana
+    validates :first_kana
+    validates :birthday
+    VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze 
+    validates :password, format: { with: VALID_PASSWORD_REGEX }
+    VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/
+    validates :last_name, format: { with: VALID_NAME_REGEX }
+    validates :first_name, format: { with: VALID_NAME_REGEX }
+    VALID_NAME_KANA_REGEX = /\A[ァ-ヶー－]+\z/
+    validates :last_kana, format: { with: VALID_NAME_KANA_REGEX }
+    validates :first_kana, format: { with: VALID_NAME_KANA_REGEX }  
+  end
+
+
+
+
+
 end

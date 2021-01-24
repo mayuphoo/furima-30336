794e744fa2daab3894c452762213d551794b5892
mayuphoo <76111639+mayuphoo@users.noreply.github.com>
2021-01-24 11:20:46 +0900

    コントローラーの定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,23 @@
 class ItemsController < ApplicationController
   def index
+    # @items = Item.order("created_at DESC")
   end
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      reedirect_to root_path
+    else
+      render :new
+    end
+  end
+
+private
+  def item_params
+    params.require(:item).permit(:name, :description, :price, :category_id, :status_id, :shopping_fee_id, :prefecture_id, :delivery_days_id, :user_id)
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
  extend ActiveHash::Associations::ActiveRecordExtensions
     belongs_to :category
-    belongs_to :delivery_day
+    belongs_to :delivery_days
     belongs_to :prefecture
     belongs_to :shopping_fee
     belongs_to :status
@@ -15,7 +15,7 @@ class Item < ApplicationRecord
 
   with_options numericality: { other_than: 1 } do
     validates :category_id
-    validates :delivery_day_id
+    validates :delivery_days_id
     validates :prefecture_id
     validates :shopping_fee_id
     validates :staus_id

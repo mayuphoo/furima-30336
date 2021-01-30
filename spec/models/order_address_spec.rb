b95834ac1bc685ef9fe262ae1c4d2dae407647fe
mayuphoo <76111639+mayuphoo@users.noreply.github.com>
2021-01-30 15:57:05 +0900

    Rubocopの実行

--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -5,11 +5,10 @@ RSpec.describe OrderAddress, type: :model do
     before do
       @user = FactoryBot.create(:user)
       @item = FactoryBot.create(:item)
-      @order_address = FactoryBot.build(:order_address, user_id: @user.id , item_id: @item.id)
-     end
-  
-    context '商品購入ができる時' do
+      @order_address = FactoryBot.build(:order_address, user_id: @user.id, item_id: @item.id)
+    end
 
+    context '商品購入ができる時' do
       it 'すべての値が正しく入力されていれば保存できること' do
         expect(@order_address).to be_valid
       end
@@ -20,7 +19,7 @@ RSpec.describe OrderAddress, type: :model do
       end
 
       it '建物名がなくても保存できる' do
-        @order_address.building_name = ""
+        @order_address.building_name = ''
         expect(@order_address).to be_valid
       end
     end

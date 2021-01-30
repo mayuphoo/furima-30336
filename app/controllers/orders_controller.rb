c506c363a65ca5c3ef3eca8afbf16608ab43395e
mayuphoo <76111639+mayuphoo@users.noreply.github.com>
2021-01-30 14:15:35 +0900

    プルリクエスト修正２

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,5 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, only: [:index]
-  # before_action :self_move_to_root, only: [:index]
   before_action :move_to_root, only: [:index]
 
   def index
